6f42442e35ee54956cb98920fdf1762d669540c9
Fysheep <121219045+Fysheep@users.noreply.github.com>
2024-04-22 19:42:05 +0200

    Latest Push

--- content.js
+++ content.js
@@ -1,6 +1,1 @@
-if (!location.href.match(/.*:\/\/.*moodle\..*/gm)) {
-  document.addEventListener("DOMContentLoaded", function () {
-    document.body.classList.add(["MPdisabled"]);
-    console.log("running");
-  });
-}
+//No code yet
